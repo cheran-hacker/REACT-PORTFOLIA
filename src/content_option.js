634c1db3ca226954c9b9c989f211e24f195d4bc1
cheran-hacker <cheransuresh869@gmail.com>
2025-09-22 15:03:26 +0530

    first commit

--- src/content_option.js
+++ src/content_option.js
@@ -145,10 +145,10 @@ const dataportfolio = [{
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    // creat an emailjs.com account 
+    YOUR_EMAIL: "cheransuresh869@gmail.com",
+    YOUR_FONE: "7305362008",
+    description: "Thiruvengadam Nagar,panapakkam-631052",
+    // create an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
     YOUR_TEMPLATE_ID: "template_id",
