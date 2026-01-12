4f9cf220791f778343c0cd6bf1d84dcb85a8ff2a
cheran-hacker <cheransuresh869@gmail.com>
2026-01-12 11:48:28 +0530

    second commit

---
+++ content_option.js
@@ -0,0 +1,177 @@
+const logotext = "CHERAN";
+const meta = {
+    title: "Cheran Portfolia",
+    description: "I’m John Doe MERN Full stack devloper,currently Persuing INFORMATION TECHNOLOGY IN BANNARI AMMAN INSTITUTE OF TECHNOLOGY",
+};
+
+const introdata = {
+    title: "I’m Cheran S",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+        fourth :"I love playing Mobiles Games"
+    },
+    description: "I am Cheran MERN STACK DEVELOPER,HTML,CSS,BOOTSTRAP EXPERT",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A Bit about my self",
+    aboutme: "I am Cheran ,Pre Final Year Student ,A MERN STACK DEVELOPER And HTML,CSS,BOOTSTRAP EXPERT."
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "Yadfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name:"Html",
+        value:98,
+    },
+    {
+        name: "Django",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 88,
+    },
+    {
+        name: "Node",
+        value: 85,
+    },
+    {
+     name:"Express",
+     value:87,   
+    },
+    {
+        name:"MongoDB",
+        value:90,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "cheransuresh869@gmail.com",
+    YOUR_FONE: "7305362008",
+    description: "Thiruvengadam Nagar,panapakkam-631052",
+    // create an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/cheran-hacker",
+    instagram:"https://www.instagram.com/cheran461/"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
